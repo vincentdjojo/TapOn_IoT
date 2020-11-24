7715788c81aceb5e4c07a50fa0d4be6f6ecb6cd8
vincentdjojo <33171330+vincentdjojo@users.noreply.github.com>
2020-11-24 17:19:23 +0800

    Add files via upload

---
+++ 02 Serial Read.py
@@ -0,0 +1,32 @@
+# Import Libraries
+import serial
+import time
+
+# Set up the Serial connection to capture the Microbit communications
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = "/dev/ttyACM0"
+ser.open()
+
+# Loop forever
+while True:
+    
+    # Read in a line from the Microbit, store it in variable 'microbitdata' as a string
+    microbitdata = str(ser.readline())
+    
+    # Cleanup the data from the microbit and convert it to an integer
+    temperature = microbitdata[2:]
+    temperature = temperature.replace(" ","")
+    temperature = temperature.replace("\\r\\n","")
+    temperature = temperature.replace("'","")
+    temperature = int(temperature)
+
+    # Print the returned unique ID from Firebase on receipt of our data
+    print("The temperature is ",temperature)
+    
+    # Pause for 5 seconds between loops
+    time.sleep(5)
+
+# Close the serial connection
+ser.close()
+
